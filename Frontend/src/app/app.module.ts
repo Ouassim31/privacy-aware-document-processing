5a87a085cc7800fb6490eb13a2b5ea7bab3f439e
Janice Jaksiewicz <janicejak@googlemail.com>
2022-11-22 14:30:25 +0100

    #12 #11 Simple applicant and upload structure

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandlordRequestFormComponent } from './landlord-request-form/landlord-request-form.component';
 import { ApplicantResponseFormComponent } from './applicant-response-form/applicant-response-form.component';
 import { BasicPageComponent } from './basic-page/basic-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const routes: Routes = [
+  { path: 'home', component: BasicPageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +30,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    MatProgressBarModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
