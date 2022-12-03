f8f7edc497be6dddfc765b5470e534e265008e77
Damian Jaspar <damianjaspar@eduroam-141-23-154-227.wlan.tu-berlin.de>
2022-12-03 12:16:16 +0100

    Added landlord calls

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,15 @@ const dataRouter = require('./routes/data'); //Import routes for data
 
 var app = express();
 
+// Set up mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb://127.0.0.1:27017';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- Backend/controllers/landlordController.js
+++ Backend/controllers/landlordController.js
@@ -1,17 +1,37 @@
 const Landlord = require("../models/landlord");
-
+const async = require("async");
 
 // Index - for now not specified
 exports.index = (req, res) => {
-    res.send("NOT IMPLEMENTED: Index of Backend");
+    res.send("INDEX");
   };
 
 // Return Landlord _id on GET with identifier as argument.
-exports.landloard_loadId_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: Landlord loadId GET");
-  };
-  
+exports.landloard_loadId_get = function (req, res, next) {
+  Landlord.findOne(
+    { identifier: req.params.identifier },
+    function (err, landlord) {
+      // Do something if there is an err.
+      if (err) {
+        return next(err);
+      }
+      // On success, return landlord _id
+      res.send(landlord._id);
+    }
+  );
+};
+
 // Handle Landlord create on POST.
-exports.landlord_create_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: Landlord create POST");
+exports.landlord_create_post = function (req, res, next) {
+  Landlord.create(
+    { identifier: req.params.identifier },
+    function (err, landlord) {
+      // Do something if there is an err.
+      if (err) {
+        return next(err);
+      }
+      // On success, return landlord _id
+      res.send(landlord._id);
+    }
+  );
 };
