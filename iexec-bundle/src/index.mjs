30f3b7b3fba3d22888de428076dc7629baceaa4e
31Ouassim <31Spoonerstreet31@gmail.com>
2022-12-13 11:07:08 +0100

    changed index js to mjs

--- iexec-bundle/src/index.mjs
+++ iexec-bundle/src/index.mjs
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
 import { BrowserRouter } from 'react-router-dom';
 
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
       
 const root = ReactDOM.createRoot(document.getElementById('root'));
