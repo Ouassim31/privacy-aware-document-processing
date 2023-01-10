3a3548f035574c81cf7f1bb70222036aa4193ce7
c.schahn <2608-c.schahn@users.noreply.git.tu-berlin.de>
2023-01-10 18:28:22 +0100

    feature: implement dataset encryption and upload

--- Frontend/src/applicantDashboard/applicantDashboard.jsx
+++ Frontend/src/applicantDashboard/applicantDashboard.jsx
@@ -16,7 +16,7 @@ function ApplicantDashbord(props) {
   const [selectedFile, setSelectedFile] = useState("");
   // Log into NFT.Storage via GitHub and create API token
   // Paste your NFT.Storage API key into the quotes:
-  const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEM2NDI5Qjg3QjM0ODE2NDg2Y2I1N2U5MzQyQ0NCMjQyRTU4NjExRjciLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3MDkyNjYxOTczMSwibmFtZSI6IklPU0wifQ.dTPIU8YNbQ-oUyfbxJUOaaGNycF3Y238yRkpnmU2pfI';
+  const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQ2OTVCQkE5MzRFYjI4RTY1OTFEQ2NiZjlCOTU0ZTY0MDAwMzVhM0YiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY3MzM1Njc5MjY2OSwibmFtZSI6Imlvc2wifQ.OoOHlZLMuVIVKKedY4XOQHyvw-ygLnj6EYcUd7314B4';
 
   const {setFileLink,uploadtoIPFS} = props
   const {pid} = useParams()
@@ -40,20 +40,92 @@ function ApplicantDashbord(props) {
     connect();
   }, []);
 
-  
+
+  async function encryptDataset(datasetFile) {
+    
+    // convert file to bytestream
+    const fileBytes = await new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(datasetFile);
+      fileReader.onload = (e) => resolve(e.target.result);
+      fileReader.onerror = () =>
+        reject(Error(`Failed to read file: ${fileReader.error}`));
+      fileReader.onabort = () => reject(Error(`Failed to read file: aborted`));
+    });
+
+    const iexec_mod = new IExec({ ethProvider: window.ethereum });
+    
+    // generate a key
+    const encryptionKey = iexec_mod.dataset.generateEncryptionKey();
+    
+    // encrypt
+    const encryptedDataset = await iexec_mod.dataset.encrypt(
+      fileBytes,
+      encryptionKey
+    );
+
+    return [new Blob([encryptedDataset]), encryptionKey];
+  };
+
 
   async function uploadToIpfs(file) {
     const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY });
     const cid = await nftstorage.storeBlob(file);
-    return 'https://' + cid + '.ipfs.nftstorage.link';
+    const uploadUrl = 'https://' + cid + '.ipfs.nftstorage.link';
+    console.log('Uploaded dataset to ' + uploadUrl);
+    return uploadUrl;
+  }
+
+
+  async function deployEncryptedDataset(encryptedDataset, ipfsAddress) {
+    const iexec_mod = new IExec({ ethProvider: window.ethereum });
+    const checksum = await iexec_mod.dataset.computeEncryptedFileChecksum(
+      encryptedDataset
+    );
+
+    const { address } = await iexec_mod.dataset.deployDataset({
+      owner: requesterAddress,
+      name: "payslip",
+      multiaddr: ipfsAddress,
+      checksum: checksum,
+    });
+
+    console.log("deployed at", address);
+    return address;
+  };
+
+
+  async function publishDataset(address) {
+    // TODO
+  }
+
+  async function signDatasetOrder(address) {
+    // TODO
+  }
+
+  async function pushDatasetSecretToSMS(datasetAddress, encryptionKey) {
+    const iexec_mod = new IExec({ ethProvider: window.ethereum });
+    const pushed = await iexec_mod.dataset.pushDatasetSecret(datasetAddress, encryptionKey);
+    console.log('secret pushed:', pushed);
+  }
+
+
+  async function createDataset(file) {
+    let encryptedKeyValue = await encryptDataset(file);
+    let encryptedDataset = encryptedKeyValue.at(0);
+    const encryptionKey = encryptedKeyValue.at(1);
+    const ipfsUrl = await uploadToIpfs(encryptedDataset);
+    // const datasetAddress = deployEncryptedDataset(encryptedDataset, ipfsAddress);
+    // pushDatasetSecretToSMS(datasetAddress, encryptionKey);
   }
 
   //handle file submit event
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const uploadUrl = await uploadToIpfs(selectedFile);
-    console.log("uploaded file to: " + uploadUrl);
-    setFileLink(pid,uploadUrl)
+    createDataset(selectedFile);
+    // const uploadUrl = await uploadToIpfs(selectedFile);
+    // console.log("uploaded file to: " + uploadUrl);
+    // setFileLink(pid,uploadUrl)
   };
 
   //handle file change event
