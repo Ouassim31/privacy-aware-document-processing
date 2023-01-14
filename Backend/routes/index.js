9bb9c3a342e8d9079bcacb845af9a935b3143628
Damian Jaspar <damianjaspar@eduroam-141-23-171-35.wlan.tu-berlin.de>
2023-01-14 16:19:10 +0100

    changed index.js route file

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res) {
-  res.redirect("/data");
+router.get("/",(req, res) => {
+  res.send("Backend Index")
 });
 
 module.exports = router;
