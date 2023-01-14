b9710a133a616053cc67147f28b01358aaa498b7
Damian Jaspar <damianjaspar@eduroam-141-23-171-35.wlan.tu-berlin.de>
2023-01-14 13:26:58 +0100

    updated contorller data model

--- Backend/models/process.js
+++ Backend/models/process.js
@@ -1,25 +1,17 @@
 const mongoose = require("mongoose");
-// require("mongoose-type-ethereum-address");
+require('mongoose-type-email');
 
 const Schema = mongoose.Schema;
 
 const ProcessSchema = new Schema({
   // Basics
-  created_on: { type: Date, immutable: true},
-  landlord_id: { type: Schema.Types.ObjectId, required:true},
-  //question: { type: String, maxLength: 200 },
-  process_state: { type: Number, default: 1, enum: [1,2,3]},
-  // iExec infos
-  //dataset_address: { type: String},
-  //applicant_address: { type: String},
-  download_address: { type: String},
-  task_id: { type: String},
-});
-
-// Virtual for process URL
-ProcessSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/${this._id}`;
+  created_on: { type: Date, immutable: true },
+  landlord_id: { type: mongoose.SchemaTypes.Email, required: true },
+  process_state: { type: Number, default: 1, enum: [1, 2, 3] },
+  description: { type: String },
+  applicant_id: { type: mongoose.SchemaTypes.Email },
+  dataset_address: { type: String },
+  task_id: { type: String },
 });
 
 // Export model
